91ab80c163d065b775ea03ce576023cd7bb3f296
anton <anton@piksolaris.ru>
2023-08-11 13:38:59 +0500

    V-110823-02 Ш-СД (Зеркальная)

--- Solaris_vedomost.py
+++ Solaris_vedomost.py
@@ -130,7 +130,7 @@ class ExampleApp(QMainWindow):
 
     def retranslateUi(self, MainWindow):
         _translate = QtCore.QCoreApplication.translate
-        MainWindow.setWindowTitle(_translate("MainWindow", "Solaris specification  V-110823-01"))
+        MainWindow.setWindowTitle(_translate("MainWindow", "Solaris specification  V-110823-02"))
         self.pushButton.setText(_translate("MainWindow", "Пуск"))
         self.pushButton2.setText(_translate("MainWindow", "Открыть файл"))
         self.pushButton3.setText(_translate("MainWindow", "Расчет материалов"))
